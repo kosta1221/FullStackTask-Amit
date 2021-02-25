feb538e776b99a8354edc957dceb7473e2d80e4c
kosta1221 <76045173+kosta1221@users.noreply.github.com>
2021-02-25 04:06:56 +0200

    test 1

--- test.test.js
+++ test.test.js
@@ -1,11 +1,24 @@
 const { app } = require("./backEnd.js");
 const puppeteer = require("puppeteer");
 const nock = require("nock");
+const useNock = require("nock-puppeteer");
 
-let browser;
+const URL = "C:/Users/kosta/Code Projcets/FullStackTask-Amit/index.html";
+const mockResponse = "success";
+
+let browser, page;
+jest.setTimeout(20000);
 
 beforeAll(async () => {
-	browser = await puppeteer.launch({ headless: false, slowMo: 30 });
+	browser = await puppeteer.launch({
+		headless: false,
+		args: ["--disable-web-security"],
+		slowMo: 30,
+	});
+	page = await browser.newPage();
+	await page.goto(URL, { waitUntil: "networkidle0" });
+
+	useNock(page, ["http://localhost:3000"]);
 });
 
 afterAll(async () => {
@@ -13,7 +26,40 @@ afterAll(async () => {
 });
 
 describe("Testing if the user can add an item", () => {
-	it("should", () => {
-		expect(5).toBe(5);
+	it("should", async () => {
+		nock("http://localhost:3000")
+			.get(/.*/)
+			.reply(200, [
+				{
+					id: "1",
+					name: "kotej",
+				},
+				{
+					id: "2",
+					name: "tomato",
+				},
+				{
+					id: "3",
+					name: "milk",
+				},
+			]);
+		nock("http://localhost:3000")
+			.post(/.*/, {
+				id: "4",
+				name: "kosta",
+			})
+			.reply(200, { id: "4", name: "kosta" });
+
+		await page.click("input");
+		await page.type("input", "kosta");
+		await page.click("button#add");
+		await page.waitForSelector(".item");
+
+		const itemText = await page.$eval("#product4 > span", (item) => item.textContent);
+		expect(itemText).toBe("kosta");
 	});
 });
+
+/* const numberOfItems = await page.$$eval(".item", (items) => {
+			items.length;
+		}); */
